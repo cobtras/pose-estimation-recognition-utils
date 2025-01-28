611db60ebe7b7090d26ef13b788e5f9547388c43
Jonas David Stephan <j.stephan@system-systeme.de>
2025-01-28 15:10:16 +0100

    Add __init__.py

---
+++ pose-estimation-recognition-utils/__init__.py
@@ -0,0 +1,9 @@
+from .MediaPipePoseNames import MediaPipePoseNames
+from .SkeletonData import SkeletonData
+from .SkeletonDataPoint import SkeletonDataPoint
+from .SkeletonDataPointWithName import SkeletonDataPointWithName
+from .SkeletonLoader import load_skeleton, load_skeleton_object
+
+__version__ = '0.1'
+__all__ = ['MediaPipePoseNames', 'SkeletonData', 'SkeletonDataPoint', 'SkeletonDataPointWithName', 'load_skeleton',
+           'load_skeleton_object']
