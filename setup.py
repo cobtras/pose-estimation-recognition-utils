145dd1d814fa0693b7ba53c99b9b02aa81b019c8
Nathalie Dollmann <dollmann.nathalie@fh-swf.de>
2025-08-04 21:06:49 +0200

    improvements and beta release 4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = read_requirements()
 
 setup(
     name='pose-estimation-recognition-utils',
-    version='0.3.0b3',
+    version='0.3.0b4',
     packages=['pose_estimation_recognition_utils'],
     install_requires=requirements,
     url='https://github.com/cobtras/pose-estimation-recognition-utils',
