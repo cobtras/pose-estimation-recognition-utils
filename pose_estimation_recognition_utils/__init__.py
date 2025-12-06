3a958f7b0ad7b8669103a96d4dba45a3aba5aa26
Jonas David Stephan <j.stephan@system-systeme.de>
2025-12-06 17:42:32 +0100

    Adding 2 variants of SkeletonDataPoint

--- pose_estimation_recognition_utils/__init__.py
+++ pose_estimation_recognition_utils/__init__.py
@@ -4,13 +4,18 @@ from .PEImage import PEImage
 from .PEVideo import PEVideo
 from .SAD import SAD
 from .Save2DData import Save2DData
+from .Save2DDataWithConfidence import Save2DDataWithConfidence
 from .Save2DDataWithName import Save2DDataWithName
+from .Save2DDataWithNameAndConfidence import Save2DDataWithNameAndConfidence
 from .SkeletonDataPoint import SkeletonDataPoint
+from .SkeletonDataPointWithConfidence import SkeletonDataPointWithConfidence
 from .SkeletonDataPointWithName import SkeletonDataPointWithName
+from .SkeletonDataPointWithNameAndConfidence import SkeletonDataPointWithNameAndConfidence
 from .VideoSkeletonData import VideoSkeletonData
 from .VideoSkeletonLoader import load_video_skeleton, load_video_skeleton_object, load_video_skeleton_from_string
 
 __version__ = '0.3.0b6'
-__all__ = ['ImageSkeletonData', 'load_image_skeleton', 'load_video_skeleton_object',
-           'SkeletonDataPoint', 'SkeletonDataPointWithName', 'VideoSkeletonData', 'load_video_skeleton',
-           'load_video_skeleton_object', 'SAD', 'Save2DData', 'Save2DDataWithName', 'PEImage', 'PEVideo']
+__all__ = ['ImageSkeletonData', 'load_image_skeleton', 'load_video_skeleton_object', 'SkeletonDataPoint',
+           'SkeletonDataPointWithConfidence', 'SkeletonDataPointWithName', 'SkeletonDataPointWithNameAndConfidence',
+           'VideoSkeletonData', 'load_video_skeleton', 'load_video_skeleton_object', 'SAD', 'Save2DData',
+           'Save2DDataWithConfidence', 'Save2DDataWithName', 'Save2DDataWithNameAndConfidence', 'PEImage', 'PEVideo']
