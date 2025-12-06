de6573d79ab6f96a44c815b7b39aedeec9fd0290
Nathalie Dollmann <dollmann.nathalie@fh-swf.de>
2025-12-06 17:55:50 +0100

    Adding all points function to ImageSkeletonLoader & VideoSkeletonLoader

--- pose_estimation_recognition_utils/__init__.py
+++ pose_estimation_recognition_utils/__init__.py
@@ -1,5 +1,5 @@
 from .ImageSkeletonData import ImageSkeletonData
-from .ImageSkeletonLoader import load_image_skeleton, load_video_skeleton_object
+from .ImageSkeletonLoader import load_image_skeleton, load_image_skeleton_object, load_image_skeleton_all_points, load_image_skeleton_object_all_points, load_image_skeleton_from_string, load_image_skeleton_from_string_all_points
 from .PEImage import PEImage
 from .PEVideo import PEVideo
 from .SAD import SAD
@@ -12,10 +12,12 @@ from .SkeletonDataPointWithConfidence import SkeletonDataPointWithConfidence
 from .SkeletonDataPointWithName import SkeletonDataPointWithName
 from .SkeletonDataPointWithNameAndConfidence import SkeletonDataPointWithNameAndConfidence
 from .VideoSkeletonData import VideoSkeletonData
-from .VideoSkeletonLoader import load_video_skeleton, load_video_skeleton_object, load_video_skeleton_from_string
+from .VideoSkeletonLoader import load_video_skeleton, load_video_skeleton_object, load_video_skeleton_from_string, load_video_skeleton_all_points, load_video_skeleton_from_string_all_points, load_video_skeleton_object_all_points
 
 __version__ = '0.3.0b6'
-__all__ = ['ImageSkeletonData', 'load_image_skeleton', 'load_video_skeleton_object', 'SkeletonDataPoint',
+__all__ = ['ImageSkeletonData', 'load_image_skeleton', 'load_image_skeleton_object', 'load_image_skeleton_all_points', 'load_image_skeleton_object_all_points',
+           'load_image_skeleton_from_string', 'load_image_skeleton_from_string_all_points', 'SkeletonDataPoint',
            'SkeletonDataPointWithConfidence', 'SkeletonDataPointWithName', 'SkeletonDataPointWithNameAndConfidence',
-           'VideoSkeletonData', 'load_video_skeleton', 'load_video_skeleton_object', 'SAD', 'Save2DData',
+           'VideoSkeletonData', 'load_video_skeleton', 'load_video_skeleton_object', 'load_video_skeleton_from_string', 'load_video_skeleton_all_points',
+           'load_video_skeleton_from_string_all_points', 'load_video_skeleton_object_all_points', 'SAD', 'Save2DData',
            'Save2DDataWithConfidence', 'Save2DDataWithName', 'Save2DDataWithNameAndConfidence', 'PEImage', 'PEVideo']
