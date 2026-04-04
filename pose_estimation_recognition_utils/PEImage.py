19813c960f86114a67df646feda1f69dd2fb505a
Jonas David Stephan <j.stephan@system-systeme.de>
2026-04-04 18:22:46 +0200

    Usage of SkeletonGraph now also in PEImage

--- pose_estimation_recognition_utils/PEImage.py
+++ pose_estimation_recognition_utils/PEImage.py
@@ -18,7 +18,7 @@ PEImage.py
 This module defines a class for saving pose estimation data of an image.
 
 Author: Jonas David Stephan
-Date: 2026-03-12
+Date: 2026-04-04
 License: Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
 """
 
@@ -27,6 +27,7 @@ import gzip
 from typing import List
 
 from .ImageSkeletonData import ImageSkeletonData
+from .SkeletonGraph import SkeletonGraph
 
 
 class PEImage:
@@ -41,10 +42,12 @@ class PEImage:
         PoseEstimationModel (str): Optional metadata
         Pose3DGenerationMethod (str): Optional metadata
         lifting_model_3d (str): Optional metadata (3DLiftingModel)
+        graph (SkeletonGraph): The static topology graph (optional).
     """
     def __init__(self, origin: str, data: ImageSkeletonData = None,
                  HumanDetectionModel: str = None, PoseEstimationModel: str = None,
-                 Pose3DGenerationMethod: str = None, lifting_model_3d: str = None):
+                 Pose3DGenerationMethod: str = None, lifting_model_3d: str = None,
+                 graph: SkeletonGraph = None):
         """
         Initialize a new PEImage instance.
 
@@ -59,6 +62,7 @@ class PEImage:
         self.PoseEstimationModel = PoseEstimationModel
         self.Pose3DGenerationMethod = Pose3DGenerationMethod
         self.lifting_model_3d = lifting_model_3d
+        self.graph = graph
 
     def set_data(self, data: ImageSkeletonData) -> None:
         """
@@ -105,6 +109,9 @@ class PEImage:
             res["Pose3DGenerationMethod"] = self.Pose3DGenerationMethod
         if self.lifting_model_3d:
             res["3DLiftingModel"] = self.lifting_model_3d
+
+        if self.graph is not None:
+            res["graph"] = self.graph.to_dict()
 
         if self.persons:
             res["persons"] = [p.to_dict() for p in self.persons]
